3f0c0d099f0890dd11032e7de4dbba437c6f3fba
pragith98 <lakshanxp@hotmail.com>
2022-12-10 01:45:37 +0530

    create employees object. create EmployeeListComponent and set props to Employee component

---
+++ src/components/employee/EmployeeList.jsx
@@ -0,0 +1,71 @@
+import Employee from "./Employee";
+
+const employees = [
+  {
+    id: 1,
+    name: {
+      title: "Mr",
+      first: "Jennie",
+      last: "Nichols",
+    },
+    location: {
+      city: "Billings",
+    },
+    dob: {
+      date: "1992-03-08T15:13:16.688Z",
+    },
+    picture: {
+      medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
+    },
+  },
+  {
+    id: 2,
+    name: {
+      title: "Mr",
+      first: "Erica",
+      last: "Gonzales",
+    },
+    location: {
+      city: "Terrace",
+    },
+    dob: {
+      date: "1992-03-08T15:13:16.688Z",
+    },
+    picture: {
+      medium: "https://randomuser.me/api/portraits/med/men/76.jpg",
+    },
+  },
+  {
+    id: 3,
+    name: {
+      title: "Mr",
+      first: "Jeremiah",
+      last: "Chapman",
+    },
+    location: {
+      city: "Sandy",
+    },
+    dob: {
+      date: "1992-03-08T15:13:16.688Z",
+    },
+    picture: {
+      medium: "https://randomuser.me/api/portraits/med/men/77.jpg",
+    },
+  },
+];
+
+const EmployeeListComponent = () => {
+  return employees.map((employee) => {
+    return <Employee employees={employee} key={employee.id}></Employee>;
+  });
+};
+
+function EmployeeList() {
+  return (
+    <ul>
+      <EmployeeListComponent></EmployeeListComponent>
+    </ul>
+  );
+}
+
+export default EmployeeList;
