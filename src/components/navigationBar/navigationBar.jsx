d9f920c623b1e4f5b00f242c7976c1a6fbd02f17
pragith98 <lakshanxp@hotmail.com>
2022-12-10 02:41:10 +0530

    add shadow

--- src/components/navigationBar/navigationBar.jsx
+++ src/components/navigationBar/navigationBar.jsx
@@ -1,6 +1,6 @@
 function NavigationBar() {
   return (
-    <nav className="navbar bg-light">
+    <nav className="navbar bg-light shadow-sm">
       <div className="container-fluid">
         <span className="navbar-brand mb-0 h1">Employees</span>
       </div>
