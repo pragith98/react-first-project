339957b3aec39dd39024297893b3171fce7fee12
pragith98 <lakshanxp@hotmail.com>
2022-12-10 03:59:53 +0530

    use states. add new employee using state

--- src/components/employee/EmployeeList.jsx
+++ src/components/employee/EmployeeList.jsx
@@ -1,69 +1,96 @@
+import { useState } from "react";
 import Employee from "./Employee";
 
-const employees = [
-  {
-    id: 1,
-    name: {
-      title: "Mr.",
-      first: "Jennie",
-      last: "Nichols",
-    },
-    location: {
-      city: "Billings",
-    },
-    dob: {
-      date: "1992-03-08T15:13:16.688Z",
-    },
-    picture: {
-      medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
-    },
-  },
-  {
-    id: 2,
-    name: {
-      title: "Mr.",
-      first: "Erica",
-      last: "Gonzales",
-    },
-    location: {
-      city: "Terrace",
-    },
-    dob: {
-      date: "1992-03-08T15:13:16.688Z",
-    },
-    picture: {
-      medium: "https://randomuser.me/api/portraits/med/men/76.jpg",
-    },
-  },
-  {
-    id: 3,
-    name: {
-      title: "Mr.",
-      first: "Jeremiah",
-      last: "Chapman",
-    },
-    location: {
-      city: "Sandy",
-    },
-    dob: {
-      date: "1992-03-08T15:13:16.688Z",
-    },
-    picture: {
-      medium: "https://randomuser.me/api/portraits/med/men/77.jpg",
-    },
-  },
-];
-
-const EmployeeListComponent = () => {
-  return employees.map((employee) => {
-    return <Employee employees={employee} key={employee.id}></Employee>;
-  });
-};
-
 function EmployeeList() {
+  const [employees, setEmployee] = useState([
+    {
+      id: 1,
+      name: {
+        title: "Mr.",
+        first: "Jennie",
+        last: "Nichols",
+      },
+      location: {
+        city: "Billings",
+      },
+      dob: {
+        date: "1992-03-08T15:13:16.688Z",
+      },
+      picture: {
+        medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
+      },
+    },
+    {
+      id: 2,
+      name: {
+        title: "Mr.",
+        first: "Erica",
+        last: "Gonzales",
+      },
+      location: {
+        city: "Terrace",
+      },
+      dob: {
+        date: "1992-03-08T15:13:16.688Z",
+      },
+      picture: {
+        medium: "https://randomuser.me/api/portraits/med/men/76.jpg",
+      },
+    },
+    {
+      id: 3,
+      name: {
+        title: "Mr.",
+        first: "Jeremiah",
+        last: "Chapman",
+      },
+      location: {
+        city: "Sandy",
+      },
+      dob: {
+        date: "1992-03-08T15:13:16.688Z",
+      },
+      picture: {
+        medium: "https://randomuser.me/api/portraits/med/men/77.jpg",
+      },
+    },
+  ]);
+
+  const EmployeeListComponent = () => {
+    return employees.map((employee) => {
+      return <Employee employees={employee} key={employee.id}></Employee>;
+    });
+  };
+
+  const AddEmployee = () => {
+    const newEmployee = {
+      id: 4,
+      name: {
+        title: "Mr.",
+        first: "Jeremiah",
+        last: "Chapman",
+      },
+      location: {
+        city: "Sandy",
+      },
+      dob: {
+        date: "1992-03-08T15:13:16.688Z",
+      },
+      picture: {
+        medium: "https://randomuser.me/api/portraits/med/men/50.jpg",
+      },
+    };
+
+    //setEmployee((employees) => employees.concat(newEmployee));
+    setEmployee((employees) => [...employees, newEmployee]);
+  };
+
   return (
     <div className="row">
       <div className="col-sm-5 mx-auto">
+        <button className="btn btn-primary mb-2" onClick={AddEmployee}>
+          Add Employee
+        </button>
         <ul className="list-group">
           <EmployeeListComponent></EmployeeListComponent>
         </ul>
